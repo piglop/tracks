ba5571e79d16554b1ad23aa4f7f0ad94c030cb87
mike <mike@bmbox.no-ip.com>
2009-01-18 17:47:37 +0100

    Fixed broken search since users don't have tags anymore

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,10 +10,8 @@ class SearchController < ApplicationController
     @found_projects = current_user.projects.find(:all, :conditions => ["name LIKE ? OR description LIKE ?", terms, terms])
     @found_notes = current_user.notes.find(:all, :conditions => ["body LIKE ?", terms])
     @found_contexts = current_user.contexts.find(:all, :conditions => ["name LIKE ?", terms])
-    # TODO: limit search to tags on todos
-    @found_tags = current_user.tags.find(:all, :conditions => ["name LIKE ?", terms])
- 
-    @count = @found_todos.size  + @found_projects.size + @found_notes.size + @found_contexts.size + @found_tags.size
+
+    @count = @found_todos.size  + @found_projects.size + @found_notes.size + @found_contexts.size
    
     init_not_done_counts
     init_project_hidden_todo_counts
